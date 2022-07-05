10a34db7573e49394b447440506fc035003cce34
Dan <72712659+zEncrypte@users.noreply.github.com>
2022-07-05 05:44:51 -0500

    Update uag.py

--- uag.py
+++ uag.py
@@ -11,7 +11,6 @@ def clear(): #clean
 clear()
 
 purple_to_red = Colors.purple_to_red
-
 Cursor.HideCursor()
 System.Title("Presiona ENTER")
 banner = r"""
@@ -32,19 +31,20 @@ banner = r"""
 """
 
 Anime.Fade(Center.Center(banner), purple_to_red, Colorate.Vertical, enter=True)
-
+System.Title("Users Agent Generator")
 Cursor.HideCursor()
 Write.Input(f'\n [+] presiona enter para iniciar...', purple_to_red, interval=0)
+clear()
 while True:
-    desktop_firefox = ua_generator.generate(device='desktop', browser='firefox')
+    Desktop_firefox = ua_generator.generate(device='desktop', browser='firefox')
     Android_chrome = ua_generator.generate(device='mobile', platform='android', browser='chrome')
     IOS_chrome = ua_generator.generate(platform=('ios', 'macos'), browser='chrome')
-    desktop_chrome = ua_generator.generate(device='desktop', browser='chrome')
+    Desktop_chrome = ua_generator.generate(device='desktop', browser='chrome')
     Android_firefox = ua_generator.generate(device='mobile', platform='android', browser='firefox')
     MacOS_safari = ua_generator.generate(platform=('ios', 'macos'), browser='safari')
     Linux_chrome = ua_generator.generate(device='desktop', browser='chrome', platform='linux')
     print(Colorate.Diagonal(purple_to_red, f""" 
-    {desktop_firefox}
+    {Desktop_firefox}
     {Android_chrome}
     {IOS_chrome}
     {desktop_chrome}
@@ -52,10 +52,10 @@ while True:
     {MacOS_safari}
     {Linux_chrome}"""))
     f = open('useragents.txt', "a+")
-    f.write(f'{desktop_chrome}\n')
+    f.write(f'{Desktop_chrome}\n')
     f.write(f'{Android_chrome}\n')
     f.write(f'{IOS_chrome}\n')
-    f.write(f'{desktop_firefox}\n')
+    f.write(f'{Desktop_firefox}\n')
     f.write(f'{Android_firefox}\n')
     f.write(f'{MacOS_safari}\n')
     f.write(f'{Linux_chrome}\n')
